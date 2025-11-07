aea6d7786267a3fce2a364c0e312240a92dde7df
Lucille Delisle <lucille.delisle@unige.ch>
2025-11-07 16:18:29 +0100

    do not hide the colorbar for the gene scale and do not show the displayModeBar on the DotPlot

--- utils.R
+++ utils.R
@@ -182,7 +182,7 @@ GetExpressionPlot <- function(inputDataList, inputDataIndex, inputGene, inputWid
       xaxis = x_ax,
       yaxis = y_ax
     ) %>% 
-    hide_colorbar() %>% 
+    # hide_colorbar() %>% 
     config(displayModeBar = F)
 
     return(p)
@@ -264,7 +264,7 @@ GetDotPlot <- function(inputDataList, inputDataIndex, inputGeneList, inputWidth,
       yaxis = y_ax,
       xaxis = x_ax,
       font = t
-    ) #%>% config(displayModeBar = F)
+    ) %>% config(displayModeBar = F)
 
     return(p)
   }
