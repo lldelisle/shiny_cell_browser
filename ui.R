6a5ca586458ac5e8bd7c5f2ea4233bd2db013c9e
Lucille Delisle <lucille.delisle@unige.ch>
2025-11-14 10:26:22 +0100

    revert the change in width

--- ui.R
+++ ui.R
@@ -59,18 +59,18 @@ ui <- fluidPage(
                             )
               ),
               mainPanel(fluid = TRUE, width = 10,
-                        fluidRow(width = 10,
-                                  column(width = 3,
+                        fluidRow(width = 12,
+                                  column(width = 4,
                                         plotlyOutput(outputId = "cluster_plot", width = "100%", height = "auto")
                                   ),
-                                  column(width = 3,
+                                  column(width = 4,
                                         plotlyOutput(outputId = "expression_plot", width = "100%", height = "auto")
                                   ),
-                                  column(width = 3,
+                                  column(width = 4,
                                         plotlyOutput(outputId = "dot_plot", width = "100%", height = "auto")
                                   )
                         ),
-                        fluidRow(width = 10,
+                        fluidRow(width = 12,
                                   textOutput('cluster_gene_table_title'),
                                   br(),
                                   DTOutput('cluster_gene_table')
